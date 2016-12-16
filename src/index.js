0b7517279e05adf31ecda53f283648196fc27c91
Angie <angi3yang@gmail.com>
2016-12-16 01:13:54 -0500

    nned more refactoring

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBtdkanoGjObuvM5e7fK5ioKU2WaTaZ7Mk';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data);
+});
 
 // Create Angies first react component.
 // This component should compile to HTML.
