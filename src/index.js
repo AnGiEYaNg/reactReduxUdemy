1fae72cb0fafc9808297cc8e9789ec8e3a533687
Angie Yang <angieyang@Angies-MacBook-Pro.local>
2016-12-17 17:17:03 -0500

    add in video list and video list item and video detail, app wont boot up need debug

--- src/index.js
+++ src/index.js
@@ -3,22 +3,43 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBtdkanoGjObuvM5e7fK5ioKU2WaTaZ7Mk';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-	console.log(data);
-});
 
 // Create Angies first react component.
 // This component should compile to HTML.
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: videos,
+			selectedVideo: null
+		}
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			})
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail videos={this.state.selectedVideo}/>
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
+
+			</div>
+		);
+	}
 }
 
 // Put this component on to the DOM.
